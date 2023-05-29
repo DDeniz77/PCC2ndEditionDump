cf92da5d37af58e113da67b65834c816d01a16ad
DDeniz77 <116292190+DDeniz77@users.noreply.github.com>
2023-05-30 00:02:05 +0300

    refactor

--- PCC2ndEdition/Chapter_4_Lists/dimensions.py
+++ PCC2ndEdition/Chapter_4_Lists/dimensions.py
@@ -1,28 +1,30 @@
-# However, sometimes you’ll want to create a list of items that cannot
-# change. Tuples allow you to do just that. Python refers to values that cannot
-# change as immutable, and an immutable list is called a tuple.
+# Sometimes you’ll want to create a list of items that cannot change.
+# Python refers to values that cannot change as immutable, and an immutable
+# list is called a tuple.
 dimensions = (200, 50)
 print(dimensions[0])
 print(dimensions[1])
-# dimensions[0] = 250 would not work as tuples are not changeable
-# Tuples are technically defined by the presence of a comma; the parentheses make them
-# look neater and more readable. If you want to define a tuple with one element, you
-# need to include a trailing comma:
-my_t = (3,)
-# It does not often make sense to build a tuple with one element, but this can happen
-# when tuples are generated automatically.
-print(my_t[0])
-print(my_t)
-# You can loop over all the values in a tuple using a for loop, just as you did with a list:
+# Tuples are technically defined by the presence of a comma; the parentheses
+# make them look neater and more readable. If you want to define a tuple with
+# one element, you need to include a trailing comma:
+my_tuple = 3,  # Notice the lack of parenthesis
+# It does not often make sense to build a tuple with one element, but this
+# can happen when tuples are generated automatically.
+print(my_tuple[0])
+print(my_tuple)
+# You can loop over all the values in a tuple using a for loop,
+# just as you did with a list:
 for dimension in dimensions:
     print(dimension)
 
 print("\nOriginal dimensions:")
 for dimension in dimensions:
     print(dimension)
-dimensions = (400, 100)
+dimensions = (400, 100)  # Redefining the tuple works however,
+# dimensions[0] = 250 would not work as tuples are not changeable
 print("\nModified dimensions:")
 for dimension in dimensions:
     print(dimension)
 # When compared with lists, tuples are simple data structures. Use them
-# when you want to store a set of values that should not be changed throughout the life of a program.
+# when you want to store a set of values that should not be changed
+# throughout the life of a program.
--- PCC2ndEdition/Chapter_4_Lists/tryityourself4-13.py
+++ PCC2ndEdition/Chapter_4_Lists/tryityourself4-13.py
@@ -3,6 +3,7 @@ foods = ("pizza", "burger", "chicken", "beef", "salmon")
 for food in foods:
     print(food)
 # foods[1] = "mayo" brackets for words no brackets for values
+print("\n")
 foods = ("cake", "burger", "chicken", "beef", "salmonella")
 for food in foods:
     print(food)
